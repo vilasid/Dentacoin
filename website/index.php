74fcf632605864d7b133c665981d8c0c2e9e28a8
Jeremias <jeremias@Jeremiass-MacBook-Pro.local>
2017-03-22 10:23:06 +0100

    upload dApp
    /account is on mainnet for use with Metamask or local node
    /testnet is on Ropsten for use with Status.im
    /uport is on Ropsten for use with uPort

---
+++ website/index.php
@@ -0,0 +1,390 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" type="image/x-icon" href="web/img/favicon.ico">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Dentacoin</title>
+        <!-- google fonts -->
+				<link href="http://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+        <!-- Bootstrap -->
+        <link href="web/css/bootstrap.min.css" rel="stylesheet">
+        <link href="web/css/font-awesome.min.css" rel="stylesheet">
+        <link href="web/css/plyr.css" rel="stylesheet">
+        <link href="web/css/style.css" rel="stylesheet">
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+    	<header>
+	        <nav class="navbar navbar-default navbar-fixed-top navbar-top">
+	            <div class="container">
+	                <div class="navbar-header">
+	                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainMenu">
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                    </button>
+	                    <a class="navbar-brand logo" href="#">
+	                    	<img src="web/img/logo.png" alt="">
+	                    </a>
+	                </div>
+	                <div>
+
+	                    <div class="collapse navbar-collapse" id="mainMenu">
+	                        <ul class="nav navbar-nav navbar-right main-menu">
+	                            <li><a href="#what">What</a></li>
+	                            <li><a href="#how">How</a></li>
+	                            <li><a href="#who">Who</a></li>
+	                            <li><a href="">Blog</a></li>
+															<li><a href="/account">Account</a></li>
+	                        </ul>
+	                    </div>
+	                </div>
+        			<button class="btn btn-default btn-lg scroll-top">
+							<i class="fa fa-chevron-up" aria-hidden="true"></i>
+					</button>
+	            </div>
+	        </nav>
+        </header>
+
+		<!-- #about -->
+        <div id="about" class="container about">
+        	<div class="row">
+        		<div class="col-md-10 col-md-offset-1">
+        			<h1 class="text-center">Dentacoin: Health Care Industry should be in the hands of the people.</h1>
+        		</div>
+        	</div>
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 clearfix">
+					<p class="lead about-text">
+						Dentacoin is an industrial Blockchain concept invented by Dentacoin Foundation in the Netherlands. <br><br>
+
+						Our mission is to contribute to improving dental health and equal access to high quality dental care to all people, through giving the power in the hands of the people. <br><br>
+
+						Through this game-changing concept, patients can use their experience and feedback to impact on improving the dental treatment quality while simultaneously reducing the costs. <br><br>
+
+						The Dentacoin ERC20 token is configured to be used globally by all individuals.  <br><br>
+						<a href="" class="learn-more">Learn more</a>
+						<span class="more-text hidden">
+							Dentacoin Fundation, the DCN inventor, bought 100% of Dentaprime International PLCs’ shares thus the disposition over the company will be in the hands of Dentacoin Foundation and will be connected to the blockchain. Each DCN represents and participates in the company’s shares and dividends. The DCN community will vote for important decisions of the company through an integrated voting system. <br><br>
+										
+							Further projects are planned:<br>
+							The health data base: In addition to the currency (DCN), the Ethereum blockchain will securely handle all private patient data. Further data will be updatable and accessible to patients.
+							The Insurance: Following treatment, Dentaprime will offer each patient a DCN paid insurance policy for lifelong free dental procedures to prevent all the health consequences related to old-age poverty.<br><br>
+					
+							Dentacoin will develop as both, a stable growing international currency and an attractive alternate derivate system for investors.			
+						</span>
+					</p>
+
+					<p class="text-center">
+						<a href="#how" class="btn btn-default btn-lg text-center get-dentacoins">Get Dentacoins</a>
+				  	<!-- <a href="trade/index.html" class="btn btn-default btn-lg text-center get-dentacoins">Get Dentacoins</a> -->
+					</p>
+				</div>
+			</div>
+
+
+    	</div>
+		<!-- /#about -->
+
+		<!-- #what -->
+        <div id="what" class="what">
+        	<div class="container">
+            	<h2 class="text-center scrollme animateme"
+					data-when="enter"
+					data-from="0.3"
+					data-to="0"
+					data-easing="easeinout"		
+					data-opacity="0"
+					data-scale="1.5"
+					data-translatey="150"
+            	>What</h2>
+        	</div>
+    		<div class="container-fluid">
+            	<div class="row">
+            		<div class="col-lg-8 col-lg-offset-2">
+            			<div id="video1" class="player scrollme animateme"
+							data-when="enter"
+							data-from="0.3"
+							data-to="0"
+							data-easing="easeinout"		
+							data-opacity="0"
+							data-scale="1.1"
+							data-translatey="150"
+            			>
+	                    	<video poster="web/media/poster.png" controls>
+		                        <source src="web/media/video1.mp4" type="video/mp4">
+		                        <source src="web/media/video1.webm" type="video/webm">
+		                        <!-- Captions are optional -->
+		                        <track kind="captions" label="English captions" src="web/media/captions.vtt" srclang="en" default>
+	                    	</video>
+	                	</div>
+            		</div>
+            	</div>
+    		</div>
+            <div class="container">
+            	<div class="row">
+
+            		<div class="col-md-4 text-center feature scrollme animateme"
+						data-when="enter"
+						data-from="0.5"
+						data-to="0"
+						data-easing="easeinout"		
+						data-opacity="0"
+						data-scale="1.2"
+						data-translatex="-200"
+            		>
+            			<i class="fa fa-globe" aria-hidden="true"></i>
+            			<h3 class="h4">Make dentistry affordable worldwide</h3>
+            			<p>
+            				As each DCN represents and participates in the company shares and dividends, the DCN owners will be able to make all the important decisions through an integrated voting system.
+            			</p>
+            			<a href="#contact" class="btn btn-default btn-lg">More</a>
+            		</div>
+
+            		<div class="col-md-4 text-center feature scrollme animateme"
+						data-when="enter"
+						data-from="0.5"
+						data-to="0"
+						data-easing="easeinout"		
+						data-opacity="0"
+						data-scale="1.2"
+						data-translatey="200"
+            		>
+            			<i class="fa fa-hdd-o" aria-hidden="true"></i>
+            			<h3 class="h4">Keep your health data safe</h3>
+            			<p>
+            				All your general health-related and specific surgical patient data, as well as the digital treatment planning and dental restoration production can now be securely stored, accessible and updatable.
+            			</p>
+            			<a href="#contact" class="btn btn-default btn-lg">More</a>
+            		</div>
+
+            		<div class="col-md-4 text-center feature scrollme animateme"
+						data-when="enter"
+						data-from="0.5"
+						data-to="0"
+						data-easing="easeinout"		
+						data-opacity="0"
+						data-scale="1.2"
+						data-translatex="200"
+            		>
+            			<i class="fa fa-credit-card" aria-hidden="true"></i>
+            			<h3 class="h4">Get a DCN-paid lifelong insurance</h3>
+            			<p>
+            				As a patient of Dentaprime, you will be offered a DCN-paid insurance policy for lifelong free dental procedures to prevent all the health consequences related to old-age poverty.
+            			</p>
+            			<a href="#contact" class="btn btn-default btn-lg">More</a>
+            		</div>
+
+            	</div>
+
+        	</div>
+        	<!-- /.container -->
+        </div>
+		<!-- /#what -->
+
+		<!-- #how -->
+        <div id="how" class="container how">
+            <h2 class="text-center scrollme animateme"
+				data-when="enter"
+				data-from="0.3"
+				data-to="0"
+				data-easing="easeinout"		
+				data-opacity="0"
+				data-scale="1.5"
+				data-translatey="50"
+            >How</h2>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-md-12 col-md-offset-0 scrollme animateme" 
+							data-when="enter"
+							data-from="0.5"
+							data-to="0"
+							data-easing="easeinout"		
+							data-opacity="0"
+							data-scale="1.2"
+							data-translatex="-200"
+						>
+							<ol class="steps">
+								<li>
+									Earn Dentacoins as a patient <br>
+									As a patient of a Dentacoin partner clinic you will be rewarded for helping them with feedback surveys, writing online reviews or referring them to a friend. Earn Dentacoins by helping improve the quality of dental services worldwide! <a href="#who" class="link-who">Dentacoin partner clinics</a>.
+								</li>
+								<li>
+									Spread the word and earn Dentacoins <br>
+									You intend to publish an article, a blog post or a comment about Dentacoin? We appreciate and reward all relevant references that spread our idea, current state and future projects. We will be happy to provide you with all the information needed.
+								</li>
+								<li>
+									Buy Dentacoins <br>
+									Buy Dentacoins for Ether (ETH) right here in your <a href="http://dentacoin.com/account/">Account</a>. You don’t have Ether? Get some <a href="https://www.kraken.com/" target="_blank">here</a>. Do you already own other cryptocurrencies like Augur, ICONOMI or Digix Global? Trade Dentacoins against most other Ethereum based tokens via <a href="https://etherdelta.github.io/#ETH-DCN" target="_blank">EtherDelta exchange</a>.
+								</li>
+							</ol>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 scrollme animateme"
+						data-when="enter"
+						data-from="0.5"
+						data-to="0"
+						data-easing="easeinout"		
+						data-opacity="0"
+						data-scale="1.2"
+						data-translatex="200"
+				>
+	                <div id="video2" class="player">
+	                    <video poster="web/media/poster.png" controls>
+	                        <source src="web/media/video1.mp4" type="video/mp4">
+	                        <source src="web/media/video1.webm" type="video/webm">
+	                        <!-- Captions are optional -->
+	                        <track kind="captions" label="English captions" src="web/media/captions.vtt" srclang="en" default>
+	                    </video>
+	                </div>
+				</div>
+			</div>
+        </div>
+		<!-- /#how -->
+
+		<!-- #who -->
+        <div id="who" class="who">
+        	<div class="container">
+	            <h2 class="text-center scrollme animateme"
+					data-when="enter"
+					data-from="0.3"
+					data-to="0"
+					data-easing="easeinout"		
+					data-opacity="0"
+					data-scale="1.5"
+					data-translatey="50"
+	            >Who</h2>
+	       		<div class="row partners">
+	       			<div class="col-md-5 col-md-offset-0 col-xs-10 col-xs-offset-1 text-center">
+	       				<a href="https://www.dentaprime.com" target="_blank" class="scrollme animateme"
+							data-when="enter"
+							data-from="0.5"
+							data-to="0"
+							data-easing="easeinout"		
+							data-opacity="0"
+							data-scale="1.2"
+							data-translatex="-200"
+	       				>
+	       					<img src="web/img/swiss-dentaprime.png" alt="" class="img-responsive">
+	       				</a>
+	       			</div>
+	       			<div class="col-md-5 col-md-offset-2 col-xs-10 col-xs-offset-1 text-center push-top-md">
+	       				<a href="http://www.dentaprime-academy.com" target="_blank" class="scrollme animateme" 
+							data-when="enter"
+							data-from="0.5"
+							data-to="0"
+							data-easing="easeinout"		
+							data-opacity="0"
+							data-scale="1.2"
+							data-translatex="200"
+	       				>
+	       					<img src="web/img/academy.png" alt="" class="img-responsive">
+	       				</a>
+	       			</div>
+	       		</div>
+            </div>
+        </div>
+		<!-- /#who -->
+
+		<!-- #contact -->
+        <div id="contact" class="container contact">
+            <h2 class="text-center scrollme animateme"
+				data-when="enter"
+				data-from="1"
+				data-to="0"
+				data-easing="easeinout"		
+				data-opacity="0"
+				data-scale="1.5"
+				data-translatey="50"
+            >Contact</h2>
+            <div class="row">
+				<div class="col-sm-6 col-sm-offset-3">
+					<ul class="social nav">
+						<li class="scrollme animateme" 
+							data-when="enter"
+							data-from="0.8"
+							data-to="0"
+							data-easing="easeinout"		
+							data-opacity="0"
+							data-scale="1.2"
+							data-translatex="-200"
+							data-rotatez="360"
+						>
+							<a href="mailto:admin@dentacoin.com" target="_blank" class="ml">
+								<i class="fa fa-envelope-square" aria-hidden="true"></i>
+								<span class="mask">
+									<span class="tip">admin@dentacoin.com</span>
+								</span>
+							</a>
+						</li>
+						<li class="scrollme animateme" 
+							data-when="enter"
+							data-from="0.8"
+							data-to="0"
+							data-easing="easeinout"		
+							data-opacity="0"
+							data-scale="1.2"
+							data-translatey="200"
+							data-rotatez="360"
+						>
+							<a href="https://www.facebook.com/dentacoin/" target="_blank" class="fb">
+								<i class="fa fa-facebook-square" aria-hidden="true"></i>
+								<span class="mask">
+									<span class="tip">Facebook</span>
+								</span>
+							</a>
+						</li>
+						<li class="scrollme animateme" 
+							data-when="enter"
+							data-from="0.8"
+							data-to="0"
+							data-easing="easeinout"		
+							data-opacity="0"
+							data-scale="1.2"
+							data-translatex="200"
+							data-rotatez="360"
+						>
+							<a href="javascript:;" target="_blank" class="tw">
+								<i class="fa fa-twitter-square" aria-hidden="true"></i>
+								<span class="mask">
+									<span class="tip">Twitter</span>
+								</span>
+							</a>
+						</li>
+					</ul>
+				</div>
+            </div>
+        </div>
+		<!-- /#contact -->
+
+		<footer>
+			<div class="container">
+				<p class="text-center signature">Design and Development: <a href="mailto:boris.nekezov@gmail.com">Boris Nekezov</a> All Rights Reserved &copy;</p>
+			</div>
+		</footer>
+
+
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="web/js/jquery-3.1.1.min.js"></script>
+        <script src="web/js/jquery-migrate-3.0.0.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="web/js/bootstrap.min.js"></script>
+        <!-- Plyr core script -->
+        <script src="web/js/plyr.js"></script>
+        <!-- ScrollMe -->
+        
+        <script src="web/js/functions.js"></script>
+        <script src="web/js/jquery.scrollme.min.js"></script>
+    </body>
+</html>
