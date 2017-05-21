aaba90a948edad0b2ee0f7646c146a962423425b
Dentacoin <jeremias@jeremiass-mbp.home>
2017-05-21 08:28:28 +0200

    added smart contract frame

---
+++ feedback network/feedback.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.4.8;
+
+
+
+/**
+ * Dentacoin extended ERC20 token contract created on February the 14th, 2017 by Dentacoin B.V. in the Netherlands
+ *
+ * For terms and conditions visit https://dentacoin.com
+ */
+
+
+
+contract owned {
+    address public owner;
+
+    function owned() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        if (msg.sender != owner) throw;
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner {
+        if (newOwner == 0x0) throw;
+        owner = newOwner;
+    }
+}
+
+
+
+
+/**
+ * Overflow aware uint math functions.
+ */
+contract SafeMath {
+  //internals
+
+  function safeMul(uint a, uint b) internal returns (uint) {
+    uint c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+
+  function safeSub(uint a, uint b) internal returns (uint) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function safeAdd(uint a, uint b) internal returns (uint) {
+    uint c = a + b;
+    assert(c>=a && c>=b);
+    return c;
+  }
+
+  function assert(bool assertion) internal {
+    if (!assertion) throw;
+  }
+}
